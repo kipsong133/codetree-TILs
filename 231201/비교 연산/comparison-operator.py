89310d73aabd68b561ea7b26db71870d93950715
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-01 18:47:39 +0900

    Title: 비교 연산, Time: 144ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 231201/비교 연산/comparison-operator.py
@@ -0,0 +1,12 @@
+def solution():
+    input_arr = list(map(int, input().split(" ")))
+    a = input_arr[0]
+    b = input_arr[1]
+    print(1 if a >= b else 0)
+    print(1 if a > b else 0)
+    print(1 if b >= a else 0)
+    print(1 if b > a else 0)
+    print(1 if a == b else 0)
+    print(1 if a != b else 0)
+
+solution()
