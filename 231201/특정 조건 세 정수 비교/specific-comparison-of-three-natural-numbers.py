181d0e1159d7619fb7daf258cc029ec41f03f38a
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-01 19:06:46 +0900

    Title: 특정 조건 세 정수 비교, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231201/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,14 @@
+def solution():
+    input_arr = list(map(int, input().split(" ")))
+    a = input_arr[0]
+    b = input_arr[1]
+    c = input_arr[2]
+
+    is_minimum: bool = (a < b) and (a < c)
+    same_numbers: bool = (a == b == c)
+
+    first_answer = 1 if is_minimum else 0
+    second_answer = 1 if same_numbers else 0
+    print(f"{first_answer} {second_answer}")
+
+solution()
