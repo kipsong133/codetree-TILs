0beedc58d8ad261a66bdbcfc06d9e374eee30735
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-01 18:44:02 +0900

    Title: 3 또는 5의 배수, Time: 128ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 231201/3 또는 5의 배수/multiples-of-3-or-5.py
@@ -0,0 +1,15 @@
+def solution():
+    input_num = int(input())
+
+    if input_num % 3 == 0:
+        print("YES")
+    else:
+        print("NO")
+
+    if input_num % 5 == 0:
+        print("YES")
+    else:
+        print("NO")
+
+
+solution()
