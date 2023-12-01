9464630b660a9c8734b77642fc44c52f4a2aafe5
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-01 19:22:36 +0900

    Title: 세 정수의 최솟값, Time: 228ms, Memory: 24MB, Status: Passed - CodeTree

--- 231201/세 정수의 최솟값/minimum-of-three-numbers.py
+++ 231201/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -1,12 +1,12 @@
 def solution():
     # Input: 8 -5 10
-    input_arr = list(map(int, input().split(" ")))
-    a = input_arr[0]
-    b = input_arr[1]
-    c = input_arr[2]
+    input_arr = list(input().split(" "))
+    a = int(input_arr[0])
+    b = int(input_arr[1])
+    c = int(input_arr[2])
 
     minimum = a
-    for num in input_arr:
+    for num in [a, b, c]:
         if minimum > num:
             minimum = num
     print(minimum)
