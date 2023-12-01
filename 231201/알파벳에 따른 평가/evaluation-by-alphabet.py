79dbc5210a8befa295a90a484e2e2a70fd2ca9ac
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-01 17:56:24 +0900

    Title: 알파벳에 따른 평가, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231201/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,16 @@
+def solution():
+    input_key = input() # str
+    result = converFrom(input_key)
+    print(result)
+
+def converFrom(key: str):
+    dic = {
+        "S": "Superior",
+        "A": "Excellent",
+        "B": "Good",
+        "C": "Usually",
+        "D": "Effort"
+    }
+    return dic.get(key, "faliure")
+
+solution()
