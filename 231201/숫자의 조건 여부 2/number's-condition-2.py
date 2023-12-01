155f63029d2250f7e87e55924b673f489c344296
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-01 18:18:39 +0900

    Title: 숫자의 조건 여부 2, Time: 125ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 231201/숫자의 조건 여부 2/number's-condition-2.py
@@ -0,0 +1,14 @@
+def solution():
+    # input: 1, 2, 3 ...
+    input_num = int(input())
+
+    # Condition: 5
+    if input_num == 5:
+        print("A")
+
+    # Condition: multiple of 2
+    if input_num % 2 == 0:
+        print("B")
+
+
+solution()
