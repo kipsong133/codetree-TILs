24434bc36758f895b017b2cf070618048e97d5d1
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-02 11:25:13 +0900

    Title: 장학금, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231202/장학금/scholarship.py
@@ -0,0 +1,20 @@
+def solution():
+    # Input: [82, 95]
+    score_arr = list(map(int, input().split(" ")))
+    mid = score_arr[0]
+    fin = score_arr[1]
+
+    # Condition 1: Midterm Exam got more than 90 score
+    if mid < 90:
+        print(0)
+        return
+
+    # Condition 2: Final Exame got more than 90 score
+    if fin >= 95:
+        print(100000)
+        return
+    if fin >= 90:
+        print(50000)
+        return    
+
+solution()
