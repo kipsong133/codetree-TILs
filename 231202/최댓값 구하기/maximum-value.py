2ddb20672582a8db4d7bb2894bf2d0075b401162
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-02 12:42:30 +0900

    Title: 최댓값 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231202/최댓값 구하기/maximum-value.py
+++ 231202/최댓값 구하기/maximum-value.py
@@ -3,23 +3,23 @@ a = input_arr[0]
 b = input_arr[1]
 c = input_arr[2]
 
-# maximum = -100
-# for num in [a, b, c]:
-#     if num > maximum:
-#         maximum = num
-if a > b:
-    if b > c:
-        print(a)
-    elif c > a:
-        print(c)
-else:
-    # b > a
-    if a > c:
-        print(b)
-    else: # c > a
-        if b > c:
-            print(b)    
-        else:
-            print(c)
+maximum = -100
+for num in [a, b, c]:
+    if num > maximum:
+        maximum = num
+# if a > b:
+#     if b > c:
+#         print(a)
+#     elif c > a:
+#         print(c)
+# else:
+#     # b > a
+#     if a > c:
+#         print(b)
+#     else: # c > a
+#         if b > c:
+#             print(b)    
+#         else:
+#             print(c)
 
-# print(maximum)
+print(maximum)
