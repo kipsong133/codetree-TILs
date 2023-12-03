88711797adbd85fc96f9ffd38f0112c09366bcc7
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-03 17:03:32 +0900

    Title: 공약수의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231203/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,25 @@
+def solution():
+    # Input: 12 14
+    input_arr = list(map(int, input().split()))
+    input_arr.sort()
+    a = input_arr[0]
+    b = input_arr[1]
+
+    common = set()
+    for num in range(1, 1920+1):
+        if 1920 % num == 0 and 2880 % num == 0:
+            common.append(num)
+    print(common)
+    
+    # Iteration: a ~ b
+    for num in range(a, b + 1):
+        # Condition: common divisor 1920 and 2880
+        if num in common:
+            print(1)
+            return
+    print(0)
+
+    
+
+
+solution()
