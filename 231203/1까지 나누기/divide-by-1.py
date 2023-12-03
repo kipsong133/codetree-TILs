b480659befd49bfe7c4ce6fc0695cb574b60e693
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-03 16:08:35 +0900

    Title: 1까지 나누기, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231203/1까지 나누기/divide-by-1.py
@@ -0,0 +1,20 @@
+def solution():
+    # input: 50
+    n = int(input())
+
+    if n == 1:
+        print(1)
+
+    cnt = 0
+    temp = n
+    # Iteration: 1 ~ n
+    for divider in range(1, n + 1):
+        if temp < 1: 
+            break
+
+        temp = int(temp / divider) # 50 
+        cnt += 1
+
+    print(cnt)
+
+solution()
