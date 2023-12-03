f49c2db26141ac1d32dfe5db13ca797fe1c6a0db
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-03 12:47:48 +0900

    Title: 369 게임, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231203/369 게임/369-game.py
+++ 231203/369 게임/369-game.py
@@ -5,24 +5,20 @@ def solution():
     # Iteration 1 ~ input+1
     n_index = 1
     while n_index < n+1:
-        str_n = str(n)
-        if len(str_n) == 1:
-            if n_index == 3 or n_index == 6 or n_index == 9:
-                print(0)
-                n_index += 1
-                continue
-                # Iteration str_n
-        else:
-            for i in range(0, len(str_n) - 1):
-                digit_str = str_n[i]
-                if digit_str == '3' or digit_str == '6' or digit_str == '9':
-                    # Condition: has '3', '6' ,'9'
-                    print(0, end = ' ')
-                    n_index += 1
-                    continue
-
+        str_n = str(n_index) # '12'
+        if has_369(str_n):
+            print(0, end = ' ')
+            n_index += 1
+            continue
         print(n_index, end = ' ')
         n_index += 1
-
-
+    
+def has_369(num: str): # '3'
+    i = 0
+    while i < len(num):
+        if num[i] == '3' or num[i] == '6' or num[i] == '9':
+            i += 1
+            return True
+        i += 1
+    return False
 solution()
