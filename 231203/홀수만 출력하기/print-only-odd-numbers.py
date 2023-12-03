429185d0eba5ce5582ce7de0c8a9b6dfc9b9239a
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-03 13:25:17 +0900

    Title: 홀수만 출력하기, Time: 164ms, Memory: 24MB, Status: Passed - CodeTree

--- 231203/홀수만 출력하기/print-only-odd-numbers.py
+++ 231203/홀수만 출력하기/print-only-odd-numbers.py
@@ -1,6 +1,11 @@
 def solution():
-    n = int(input())
-    if n % 2 == 1 and n % 3 == 0:
-        print(n)
-
+    # 몇 개 입력받을 지 정수
+    n = int(input()) # 5 개를 입력받겠다.
+    
+    # input: 2 7 3 9 4 (각각 줄바꿈) 그래서 5 개를 입력 받았다.
+    input_arr = [int(input()) for _ in range(n)]
+    for num in input_arr:
+        if num % 2 == 1 and num % 3 == 0:
+            print(num)
+            
 solution()
