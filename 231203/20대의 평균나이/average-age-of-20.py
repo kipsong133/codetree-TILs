1f74fa2bd570a2dca7266ed6ddf754b407c09de4
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-03 16:23:24 +0900

    Title: 20대의 평균나이, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 231203/20대의 평균나이/average-age-of-20.py
+++ 231203/20대의 평균나이/average-age-of-20.py
@@ -6,12 +6,10 @@ def solution():
         age = int(input())
         
         age_arr.append(age)
-        continue_input = (age < 30 and 19 age)
+        continue_input = (age < 30 and 19 < age)
 
     age_arr.pop()
     avg = sum(age_arr) / len(age_arr)
     print(f'{avg:.02f}')
-
-
 
 solution()
