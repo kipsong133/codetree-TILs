d4e4d76c05180f1747344f45f2c6b0bf4d016fe1
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-03 17:33:45 +0900

    Title: 모두 3의 배수, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 231203/모두 3의 배수/all-multiples-of-3.py
+++ 231203/모두 3의 배수/all-multiples-of-3.py
@@ -2,10 +2,9 @@ def solution():
     # input: 3 6 9 12 15
     input_arr = list(int(input()) for _ in range(5))
     
-    is_multi: bool = True
     for num in input_arr:
-        if num % 3 == 0:
-            print(1)
+        if num % 3 != 0:
+            print(0)
             return
-    print(0)
+    print(1)
 solution()
