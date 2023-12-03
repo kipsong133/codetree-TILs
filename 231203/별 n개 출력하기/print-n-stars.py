65aed93150a006b0b93f22370c51ca97ce66d236
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-03 10:24:13 +0900

    Title: 별 n개 출력하기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231203/별 n개 출력하기/print-n-stars.py
@@ -0,0 +1,13 @@
+def solution():
+    # input, 4byte int type 
+    n = int(input())
+
+    # print out "*", 1 ~ n lines
+    i = 0
+    while i < n: # O(n)
+        print("*")
+        i += 1
+
+
+
+solution()
