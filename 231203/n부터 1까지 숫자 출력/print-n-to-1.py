18c110c4d6ed9e2f2a19e63be581982f06dac15c
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-03 10:35:43 +0900

    Title: n부터 1까지 숫자 출력, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231203/n부터 1까지 숫자 출력/print-n-to-1.py
@@ -0,0 +1,13 @@
+def solution():
+    # input
+    n = int(input())
+
+    # print out number, n ~ 1
+    # start number
+    i = n
+    while i > 0:
+        print(i, end = " ")
+        i -= 1
+
+
+solution()
