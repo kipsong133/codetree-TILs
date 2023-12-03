6f8cd659c5a04ddbe3cf516b58a605a5802f427e
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-03 16:18:15 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231203/20대의 평균나이/average-age-of-20.py
@@ -0,0 +1,16 @@
+def solution():
+    continue_input = True
+    age_arr = []
+
+    while continue_input:
+        age = int(input())
+        age_arr.append(age)
+        continue_input = (age < 30)
+
+    age_arr.pop()
+    avg = sum(age_arr) / len(age_arr)
+    print(f'{avg:.02f}')
+
+
+
+solution()
