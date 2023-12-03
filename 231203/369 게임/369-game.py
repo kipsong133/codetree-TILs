e3373c431ba7778a5a6c8b22e94ba22796b447c3
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-03 12:50:49 +0900

    Title: 369 게임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231203/369 게임/369-game.py
+++ 231203/369 게임/369-game.py
@@ -5,6 +5,9 @@ def solution():
     # Iteration 1 ~ input+1
     n_index = 1
     while n_index < n+1:
+        if is_multiple_3(n_index):
+            print(0, end = ' ')
+
         str_n = str(n_index) # '12'
         if has_369(str_n):
             print(0, end = ' ')
@@ -12,6 +15,9 @@ def solution():
             continue
         print(n_index, end = ' ')
         n_index += 1
+
+def is_multiple_3(num int):
+    return num % 3 == 0
     
 def has_369(num: str): # '3'
     i = 0
