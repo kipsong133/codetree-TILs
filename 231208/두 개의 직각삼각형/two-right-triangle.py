e3fade709e4cbca37c559f991db64a2a3e80d8bf
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-08 07:55:30 +0900

    Title: 두 개의 직각삼각형, Time: 146ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231208/두 개의 직각삼각형/two-right-triangle.py
@@ -0,0 +1,29 @@
+def solution():
+    # Input: 4
+    n = int(input())
+
+    # i: * ' '
+    # 0: 8, 0
+    # 1: 6, 2
+    # 2: 4, 4
+    # 3: 2, 6
+    # * : ((n * 2) - 2 * i) 개
+    # ' ': (2 * i) 개
+
+    # Iteration: lines
+    for line in range(4, 0, -1):
+        # Iteration: star
+        for star in range(line):
+            print('*', end = '')
+        
+        # Iteration: space
+        for space in range(n * 2 - (2 * line)):
+            print(' ', end = '')
+
+        # Iteration: star
+        for star in range(line):
+            print('*', end = '')
+        
+        print('')
+    
+solution()
