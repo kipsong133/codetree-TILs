6bc1fea179c2ed2879951f75e824b62fed4008a8
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-17 10:56:29 +0900

    Title: 최소공배수 구하기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231217/최소공배수 구하기/find-the-least-common-multiple.py
+++ 231217/최소공배수 구하기/find-the-least-common-multiple.py
@@ -4,15 +4,14 @@ def find_lcm(n: int, m: int) -> int:
     result: list[int] = []
     min_value: int = min(n, m)
     for i in range(2, min_value + 1):
-        if i == min_value:
-            result.append(n)
-            result.append(m)
-
         if n % i == 0 and m % i == 0:
             n = n // i
             m = m // i 
             result.append(i)
             continue
+        if i == min_value:
+            result.append(n)
+            result.append(m)
     
     mutiple_value: int = 1 
     
