112d16ae6b06bd11fc7d812ac5e78f92aa9f710d
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-19 07:49:41 +0900

    Title: 함수를 이용한 소수 판별, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231219/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 231219/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -1,15 +1,19 @@
-# input: 5 19
-a, b = tuple(map(int, input().split()))
-
 def is_prime(n: int) -> bool:
     for i in range(2, n):
         if n % i == 0:
             return False
     return True
 
-result = 0
-# sum prime numbers between 5 and 19
-for i in range(a, b + 1):
-    if is_prime(i):
-        result += i
-print(result)
+def solution():
+    # input: 5 19
+    a, b = tuple(map(int, input().split()))
+    if a < 2 or b < 2:
+        print(0)
+        return
+
+    result = 0
+    # sum prime numbers between 5 and 19
+    for i in range(a, b + 1):
+        if is_prime(i):
+            result += i
+    print(result)
