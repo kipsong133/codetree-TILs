6fbaf7151e8b841e248578beb3047debfe81a702
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-27 08:16:08 +0900

    Title: 함수를 이용한 온전수 판별, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 231227/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
+++ 231227/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -1,26 +1,23 @@
-def is_magic_number(x: int) -> bool:
-    if x % 2 == 0:
-        return False
-
-    position: int = len(str(x))
-    first: int = int(str(x)[position - 1])
-    if first == 5:
-        return False
-
-    if x % 3 == 0:
-        return False
-
-    if x % 9 != 0:
-        return False
-
-    return True
-
-
+# 변수 선언 및 입력:
 a, b = tuple(map(int, input().split()))
 
+
+# 온전수인지 아닌지 여부를 판단하는 함수를 작성합니다.
+def judge_number(n):
+    if n % 2 == 0:
+        return False
+    elif n % 10 == 5:
+        return False
+    elif n % 3 == 0 and n % 9 != 0:
+        return False
+    else:
+        return True
+
+
 cnt = 0
+
 for i in range(a, b + 1):
-    if is_magic_number(i):
+    if judge_number(i):
         cnt += 1
 
 print(cnt)
