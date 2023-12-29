e7dbb47116ea8a36c46e99a94298b7765df6e269
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2023-12-29 16:18:51 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231229/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -0,0 +1,28 @@
+a,b = tuple(map(int, input().split()))
+cnt = 0
+
+def sum_position(num: int) -> int:
+    temp = []
+    while num != 0:
+        temp.append(num % 10)
+        num = num // 10
+    return sum(temp)
+
+def unique_num(num: int):
+  return all(num % i != 0 for i in range(2, num)) 
+
+def recursive(a: int, b: int):
+  global cnt
+  if a >= b:
+      return
+  sum_result = sum_position(a)
+  if sum_result % 2 == 0 and unique_num(a):
+    cnt += 1
+  a += 1
+  recursive(a, b)
+
+def solution():
+    recursive(a, b)
+    print(cnt)
+
+solution()
