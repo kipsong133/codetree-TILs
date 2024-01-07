2e7793e75f041340609f92ed011c148a8f2dc8a4
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2024-01-08 07:57:59 +0900

    Title: 나누고 빼면서 합하기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240108/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 240108/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -1,24 +1,17 @@
-# n개의 원소 m의 공백
 n, m = tuple(map(int, input().split())) # 5 4
-arr = list(map(int, input().split())) # [5, 4, 3, 2, 1]
+arr = [0] + list(map(int, input().split())) # 서수 표기법이 문제에서 조금 다르게 나와있어서 맞추기 위해 추가
+def get_answer():
+    global m # 4
 
-def calculateElements(num: int) -> int:
-    cnt = 0
-    while num != 1:
-        cnt += num
-        if num % 2 == 0:
-            num /= 2
+    return_value:int = 0
+    while m: # m == 1 이 되면 false가 되어서 탈출된다.
+        return_value += arr[m]
+
+        if m % 2 == 0:
+            m //= 2
         else:
-           num -= 1
-    return cnt
+            m -= 1
 
-def solution():
-    # validation
-    if (m > n):
-        return
-    
-    input_num: int = arr[m]
-    result: int = calculateElements(input_num)
-    print(result)
+    return return_value
 
-solution()
+print(get_answer())
