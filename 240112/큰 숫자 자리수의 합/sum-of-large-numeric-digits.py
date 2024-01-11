ae2eb8977073b63302322c17693c5b0a62c92431
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2024-01-12 07:55:59 +0900

    Title: 큰 숫자 자리수의 합, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240112/큰 숫자 자리수의 합/sum-of-large-numeric-digits.py
+++ 240112/큰 숫자 자리수의 합/sum-of-large-numeric-digits.py
@@ -1,18 +1,7 @@
-from functools import reduce
-
-mutiple_element = lambda x, y: x * y
-
-def sum_pos(num: int): 
+def sum_pos(num: int):
     if num < 10: return num
-
-    pos_num = num % 10
-    remainder = int(num // 10)
-    return sum_pos(remainder) + pos_num
-
+    return sum_pos(num // 10) + num % 10
 
 # input
-arr: [int] = list(map(int, input().split())) # [123, 456, 789]
-multi_result: int = reduce(mutiple_element, arr)
-
-result: int = sum_pos(multi_result)
-print(result)
+a, b, c = tuple(map(int, input().split())) # 123 456 789
+print(sum_pos(a * b * c))
