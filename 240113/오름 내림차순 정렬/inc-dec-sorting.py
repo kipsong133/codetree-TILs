24b6cb96daa377899385d47f0ace73f31e6520bf
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2024-01-13 15:46:10 +0900

    Title: 오름 내림차순 정렬, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240113/오름 내림차순 정렬/inc-dec-sorting.py
+++ 240113/오름 내림차순 정렬/inc-dec-sorting.py
@@ -1,23 +1,15 @@
-def ascend(i: int):
-    if i == n - 1: 
-        print(arr[i], end = " ")
-        return arr[i]
-    print(arr[i], end = " ")
-    return ascend(i + 1)
-
-def descend(i: int):
-    if i == 0:
-        print(arr[0], end = " ")
-        return arr[0]
-
-    print(arr[i], end = " ")
-    return descend(i - 1)
-
 # input
 n = int(input())
 arr = list(map(int, input().split()))
 arr.sort()
 
-ascend(0)
+# 오름차순
+for elem in arr:
+    print(elem, end = " ")
+
 print()
-descend(n - 1)
+
+# 내림차순
+arr.sort(reverse = True)
+for elem in arr:
+    print(elem, end = " ")
