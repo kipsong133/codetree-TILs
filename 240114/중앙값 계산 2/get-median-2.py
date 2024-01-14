38f922911e9ff17901a480c317f5823c12c30ecc
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2024-01-14 15:16:18 +0900

    Title: 중앙값 계산 2, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240114/중앙값 계산 2/get-median-2.py
+++ 240114/중앙값 계산 2/get-median-2.py
@@ -2,14 +2,11 @@
 n = int(input()) # 5
 arr = list(map(int, input().split())) # [1, 2, 3, 4, 5]
 
-result: [int] = []
-temp: [int] = []
-for i in range(0, len(arr)):
-    temp.append(arr[i]) # 1, 2, 3
-    if (i+1) % 2 == 1:
-        temp.sort()
-        mid_index = int(len(temp) / 2)
-        result.append(str(temp[mid_index]))
-
-solution = " ".join(result)
-print(solution)
+for i in range(n):
+    # 0부터 시작하니까, 짝수Index가 홀수번째 데이터임
+    if i % 2 == 0: 
+        #  arr의 홀수번쨰까지 slicing + sorted
+        sorted_arr = sorted(arr[:i + 1]) # slice마지막을 포함하기 위해 "+1"
+        # 만약 마지막 5번째면 i == 4, 그러면 중앙값은 index 상으로 2 이다.
+        # i // 2 == 2 이므로,
+        print(sorted_arr[i // 2], end = " ")
