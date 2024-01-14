4dbb1747e4dcbb3b9168b38e7c2fbb2f8bde09a9
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2024-01-15 07:32:25 +0900

    Title: 사는 지역, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240115/사는 지역/where-live.py
+++ 240115/사는 지역/where-live.py
@@ -1,22 +1,22 @@
 class Address:
-	def __init__(self, name, address, region):
-		self.name = name
-		self.address = address
-		self.region = region
+    def __init__(self, name, address, region):
+        self.name = name
+        self.address = address
+        self.region = region
+        
+        
+# 변수 선언 및 입력
+n = int(input())
+arr = [tuple(input().split()) for _ in range(n)]
+people = [Address(name, address, region) for name, address, region in arr]
 
-	def __lt__(self, other):
-		return self.name < other.name
+# 사전순으로 이름이 가장 느린 사람 찾기
+target_idx = 0
+for i, person in enumerate(people):
+    if person.name > people[target_idx].name:
+        target_idx = i
 
-cnt = int(input())
-persons: [Address] = []
-while True:
-    if len(persons) >= cnt:
-        break
-    name, address, region = tuple(input().split())
-    persons.append(Address(name, address, region))
-
-persons.sort()
-target = persons[-1]
-print(f"name {target.name}")
-print(f"addr {target.address}")
-print(f"city {target.region}")
+# 결과 출력
+print(f"name {people[target_idx].name}")
+print(f"addr {people[target_idx].address}")
+print(f"city {people[target_idx].region}")
