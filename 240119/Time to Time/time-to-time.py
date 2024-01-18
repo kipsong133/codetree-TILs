e16e324313f77bc3401be2294c38557ac12b5287
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2024-01-19 07:56:44 +0900

    Title: Time to Time, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/Time to Time/time-to-time.py
+++ 240119/Time to Time/time-to-time.py
@@ -1,18 +1,4 @@
 # input
 a, b, c, d = tuple(map(int, input().split()))
 
-def calculate_time(a,b, elapsed_time):
-    global c
-    global d
-
-    # 리턴 조건
-    if a == c and b == d:
-        return elapsed_time
-    
-    if b == 60:
-        a += 1
-        b = 0
-    
-    return calculate_time(a, b+1, elapsed_time + 1)
-    
-print(calculate_time(a, b, 0))
+print((c * 60 + d) - (a * 60 + b))
