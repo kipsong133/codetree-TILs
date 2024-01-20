a5ce661051fa9e72c948f442e216383b98f68e2c
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2024-01-20 11:12:27 +0900

    Title: Date to Date, Time: 164ms, Memory: 24MB, Status: Passed - CodeTree

--- 240120/Date to Date/date-to-date.py
+++ 240120/Date to Date/date-to-date.py
@@ -1,25 +1,22 @@
-# input
+# 변수 선언 및 입력
 m1, d1, m2, d2 = tuple(map(int, input().split()))
 
-num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-# simulating
-# m1.d1 -> m2.d2
-# elapsed_day = 0
-
-def count_days(m1, d1, m2, d2, elapsed_day): 
-    if m1 == m2 and d1 == d2:
-        return elapsed_day
+def num_of_days(m, d):
+    # 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다. 
+    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days = 0
     
-    if d1 > num_of_days[m1]:
-        m1 += 1
-        d1 = 0
-
-    return count_days(m1, d1+1, m2, d2, elapsed_day+1)
+    # 1월부터 (m - 1)월 까지는 전부 꽉 채워져 있습니다.
+    for i in range(1, m):
+        total_days += days[i]
     
+    # m월의 경우에는 정확이 d일만 있습니다.
+    total_days += d
+    
+    return total_days    
 
-# output
-if m1 == m2 and d1 == d2:
-    print(1)
-else:
-    print(count_days(m1, d1, m2, d2, 0))
+
+# 출력
+total_days = num_of_days(m2, d2) - num_of_days(m1, d1) + 1
+print(total_days)
