10bb3da72d067cfeb3ad9c0de53b4851ebb4f3a7
Woo Sung Kim <65879950+kipsong133@users.noreply.github.com>
2024-02-01 07:32:56 +0900

    Title: 여러가지 진수변환, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240201/여러가지 진수변환/various-numeral-system-transformations.py
@@ -0,0 +1,15 @@
+# input
+n, b = tuple(map(int, input().split()))
+digits = []
+
+# 이진수로 변환합니다.
+while True:
+    if n < b:
+        digits.append(n)
+        break
+    
+    digits.append(n % b)
+    n //= b
+
+for digit in digits[::-1]:
+    print(digit, end ="")
